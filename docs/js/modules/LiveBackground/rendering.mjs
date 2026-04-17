e2b56cb8f059cc0d359682dead796c2de2d8857f
Vic CG <vic.cg93@gmail.com>
2026-04-16 18:04:00 -0600

    unstable feature added: live wallpaper

--- docs/js/modules/LiveBackground/rendering.mjs
+++ docs/js/modules/LiveBackground/rendering.mjs
@@ -21,7 +21,7 @@ function adjustCanvasToWindow(canvas,ctx){
     const rect = canvas.getBoundingClientRect()
     const calculatedWidth = Math.round(rect.width * dpr)
     const calculatedHeight = Math.round(rect.height * dpr)
-    if(dprGlobal != dpr || canvas.width != calculatedWidth || canvas.height != calculatedHeight){
+    if(dprGlobal != dpr || canvas.width != calculatedWidth || canvas.height != calculatedHeight || canvasWidth != canvas.width){
         dprGlobal = dpr
         //these dimensions are canvas drawing area related, not DOM element dimensions
         canvas.width = calculatedWidth
@@ -45,22 +45,96 @@ async function loadImages(sources) {
     return Promise.all(promises);
 }
 
-async function initBackground(sources, idCanvas){
-    try{
-        bgImages = await loadImages(sources)
-        if(bgImages.length > 0){
-            let canvas = document.getElementById(idCanvas)
-            ctx = canvas.getContext('2d'/*, {willReadFrequently: true}*/)
-            adjustCanvasToWindow(canvas,ctx)
-            //drawDefaultImage(bgImages[0], ctx)
-            initialStamp = Date.now()
-            lastTimeFrame = initialStamp
-            animate()
-        }
-    } catch(err){
-        console.error("error loading named sources ", err)
+//
+
+function drawSource(ctx, source, xPos, yPos, scaleX, scaleY){
+    ctx.beginPath()
+    ctx.drawImage(source,xPos,yPos,scaleX, scaleY)
+    ctx.closePath()
+}
+
+ function initBackground(source, idCanvas){
+    let canvas = document.getElementById(idCanvas)
+    ctx = canvas.getContext('2d')
+    adjustCanvasToWindow(canvas, ctx)
+   
+    if(typeof source != 'undefined'){
+        drawSource(ctx,source,0, 0, canvasWidth, canvasHeight)
+        animateBg()
     }
 }
+
+let lastFrameTime = 0
+let updateMs = 500
+let deltaComponents = 2
+const blackUpperLimit = 60
+const washStep =4
+
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+function proccessImageSection(imageData) {
+    let imageDataProcessed = []
+    for (let index = 0; index < (imageData.length -4); index+=4) {
+        let r=imageData[index], g=imageData[index+1], b=imageData[index+2]//, a=imageData[index+3]
+        if (imageData[index]>blackUpperLimit && imageData[index+1]>blackUpperLimit && imageData[index+2]>blackUpperLimit){
+            if(imageData[index]<(255-washStep)){imageData[index]+=washStep}else{imageData[index]=255}
+            if(imageData[index+1]<(255-washStep)){imageData[index+1]+=washStep}else{imageData[index+1]=255}
+            if(imageData[index+2]<(255-washStep)){imageData[index+2]+=washStep}else{imageData[index+2]=255}
+        }else if (imageData[index]>washStep && imageData[index+1]>washStep && imageData[index+2]>washStep){
+            imageData[index]-=washStep
+            imageData[index+1]-=washStep
+            imageData[index+2]-=washStep
+        }
+    }
+}
+
+function animateBg(){
+    //logic that wash colors progressively
+    const maxLinesPerWash = 200
+    //const startLine = Math.round((canvasHeight - maxLinesPerWash) * Math.random())
+    const startLine = 10
+
+    //image washing
+    const imageData = ctx.getImageData(0,startLine,canvasWidth*dprGlobal, maxLinesPerWash)
+    delay(200).then(()=>{
+        proccessImageSection(imageData.data)
+        ctx.putImageData(imageData,0,startLine)
+    })
+        
+
+    /*
+    let angle = Math.PI*1.5
+    const now = Date.now()
+    let newPositions = {xPos: 0, yPos: 0}
+    if(now - lastFrameTime > updateMs){
+        clearCanvas(ctx)
+        newPositions = getNewPositions(xPos,yPos)
+        drawSource(ctx,source, newPositions.xPos, newPositions.yPos, canvasWidth*2, canvasHeight*2)
+        lastFrameTime = Date.now()
+    }
+    */
+    requestAnimationFrame(animateBg)
+}
+
+function getNewPositions(xPos, yPos, angle){
+    let result = {xPos: 0, yPos: 0}
+    if(angle < Math.PI/2 || angle >= Math.PI*1.5){
+        result.xPos = xPos+deltaComponents
+    }else{
+        result.xPos = xPos+deltaComponents
+    }
+    if(angle <  Math.PI/2){
+        result.yPos = yPos - deltaComponents
+    }else{
+        result.yPos = yPos + deltaComponents
+    }
+    return result
+}
+
+
+
+
+
 //images are going to be passed using event handlers
 function drawDefaultImage(image, ctx){
     //ctx.fillStyle = 'rgba(64,21,89,1)'
@@ -120,6 +194,8 @@ function animate(){
     requestAnimationFrame(animate)
 }
 
+//TODO shit to refactor 
+
 function getNewDrawingPosition(initialStamp){
     let elapsedTime = Date.now() - initialStamp
     return Math.round((-canvasHeight) + calculateDistance(elapsedTime/1000))
@@ -156,5 +232,20 @@ function clearCanvas(ctx){
     ctx.fill()
     ctx.closePath()
 }
+
+//******************************************space sizing and references */
+
+let liminalRadius = 0
+
+//adjuster is a regulator for relation between available space and radius tendency to increase
+function getAdjustedRadius(width, numberOfElements, horizontalSpacing){
+    return Math.round(((width-horizontalSpacing)/(numberOfElements*2)))
+}
+
+function calculateGapBetweenCircles(numberOfCircles, adjustedRadius, width, limitGap){
+    return (((width-limitGap)/numberOfCircles)-adjustedRadius)
+}
+
+
 
 export {initBackground}
