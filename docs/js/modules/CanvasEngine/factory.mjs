47d6dd58f2784bce4590ede3ceb11aa943f34309
viccg93 <vic.cg93@gmail.com>
2026-08-27 19:11:25 -0600

    entry point of canvas engine

---
+++ docs/js/modules/CanvasEngine/factory.mjs
@@ -0,0 +1,29 @@
+function getCanvas(idCanvas, containerWidth, containerHeight){
+    let canvas = document.getElementById(idCanvas)
+    let ctx = canvas.getContext('2d')
+    //using a obj in return statement
+    return {
+        canvas: canvas,
+        ctx: ctx,
+        globalDPR: 1,
+        widthDOM: 0,
+        heightDOM: 0,
+        adjustCanvasToWindow(){
+            const dpr = window.devicePixelRatio || 1
+            const rect = this.canvas.getBoundingClientRect()
+            const calculatedWidth = Math.round(rect.width * dpr)
+            const calculatedHeight = Math.round(rect.height * dpr)
+            //change only mechanism
+            if(dprGlobal != dpr || this.canvas.width != calculatedWidth || this.canvas.height != calculatedHeight){
+                this.globalDPR = dpr
+                //these dimensions are canvas drawing area related, not DOM element dimensions
+                this.canvas.width = calculatedWidth
+                this.canvas.height = calculatedHeight
+                this.ctx.scale(dpr,dpr)
+                //update sizing members
+                this.widthDOM = rect.width
+                this.heightDOM = rect.height
+            }
+        } 
+    }
+}
